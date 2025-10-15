2bf2231c8c04306690e8c4c5d79a6a17bc2bbdfe
Brady <96459960+bradyd78@users.noreply.github.com>
2025-10-15 16:21:02 -0500

    AppManager : addBill

---
+++ backend/AppManager.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class PoliticalAppManager {
+    private List<Bill> bills;
+    private List<PoliticalFigure> figures;
+
+    public PoliticalAppManager() {
+        bills = new ArrayList<>();
+        figures = new ArrayList<>();
+    }
+
+    // Bill management
+    public void addBill(Bill bill) {
+        bills.add(bill);
+    }
+}
