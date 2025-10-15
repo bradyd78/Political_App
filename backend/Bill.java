47a29fa3dda50c58efad5595f59153812303e00f
Brady <96459960+bradyd78@users.noreply.github.com>
2025-10-15 15:42:50 -0500

    Add Functionality to Methods

--- backend/Bill.java
+++ backend/Bill.java
@@ -1,30 +1,66 @@
 
+package backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bill {
-    private int billID
+    private int billID;
+    private String name, summary, fullBill, status;
+    private int numberMockVotes, inFavor, against;
+    private List<String> comments;
 
-    String name, summary, fullBill, status;
-    int numberMockVotes, inFavor, against;
-
-    public double showVotePercent(int billID){
-
+    public Bill(int billID, String name, String summary, String fullBill, String status) {
+        this.billID = billID;
+        this.name = name;
+        this.summary = summary;
+        this.fullBill = fullBill;
+        this.status = status;
+        this.numberMockVotes = 0;
+        this.inFavor = 0;
+        this.against = 0;
+        this.comments = new ArrayList<>();
     }
 
-    public Bill viewBill(Bill billID){
-        return billID.toString();
+    public double showVotePercent() {
+        if (numberMockVotes == 0) return 0.0;
+        return (double) inFavor / numberMockVotes * 100;
     }
 
-    public String viewCommentsOnBill(int billID){
-
+    public Bill viewBill() {
+        // Returns a copy of this bill (could be more meaningful in context)
+        return this;
     }
 
-    public void makeComment(String comment, int billID){
-
+    public List<String> viewCommentsOnBill() {
+        return new ArrayList<>(comments);
     }
 
-    public void voteOnBill(boolean vote){
-
+    public void makeComment(String comment) {
+        comments.add(comment);
     }
 
-    public static void toString(){
+    public void voteOnBill(boolean vote) {
+        numberMockVotes++;
+        if (vote) {
+            inFavor++;
+        } else {
+            against++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "billID=" + billID +
+                ", name='" + name + '\'' +
+                ", summary='" + summary + '\'' +
+                ", fullBill='" + fullBill + '\'' +
+                ", status='" + status + '\'' +
+                ", numberMockVotes=" + numberMockVotes +
+                ", inFavor=" + inFavor +
+                ", against=" + against +
+                ", comments=" + comments +
+                '}';
     }
 }
