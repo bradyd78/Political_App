db8102cd6c37b1d7302bf3eda0a29f1e994f88ed
Brady <96459960+bradyd78@users.noreply.github.com>
2025-10-15 15:38:24 -0500

    Basic Structure of Bill

--- backend/Bill.java
+++ backend/Bill.java
@@ -1,1 +1,23 @@
 
+public class Bill {
+    private int billID
+
+    String name, summary, fullBill, status;
+    int numberMockVotes, inFavor, against;
+
+    public double showVotePercent(int billID){
+
+    }
+
+    public String viewCommentsOnBill(int billID){
+
+    }
+
+    public void makeComment(String comment, int billID){
+
+    }
+
+    public void voteOnBill(boolean vote){
+
+    }
+}
