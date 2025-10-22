2129aa0a56815d0a0c4838295d31861615a374f6
Brady <96459960+bradyd78@users.noreply.github.com>
2025-10-22 16:19:55 -0500

    Update BillController.java with CreateBill method

--- backend/controller/BillController.java
+++ backend/controller/BillController.java
@@ -1,1 +1,16 @@
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
+public class BillController{
+
+  private Map<Integer, Bill> billStore = new HashMap<>();
+
+  //Create a new Bill
+  public Bill createBill(Bill bill){
+    billStore.put(bill.getBillID(), bill);
+    return bill;
+  }
+
+  
+
+}
