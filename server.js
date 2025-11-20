a01f8ee17e51726fb5368b7fac8b8ee6a2f10922
Brady <96459960+bradyd78@users.noreply.github.com>
2025-11-20 22:45:54 +0000

    npm install express cors

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Political App API is running!');
+});
+
+const PORT = process.env.PORT || 3000;
