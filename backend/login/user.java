6f0c1536d401cc088887690f020319a06a1b5ab8
Brady <96459960+bradyd78@users.noreply.github.com>
2025-12-01 13:20:01 -0600

    Add User class with constructors and methods

--- backend/login/user.java
+++ backend/login/user.java
@@ -1,1 +1,40 @@
+public class User {
+    private String username;
+    private String password;
+
+    //No-args constructor
+    public User() {
+    }
+
+    //constructor
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    //Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    //never expose raw passwords in real apps
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User {" +
+                "username='" + username + '\'' +
+                ", password='[PROTECTED]'}";
+    }
+}
 
